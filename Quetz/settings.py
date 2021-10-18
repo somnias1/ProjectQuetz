979ee91854c6dd5113c370c2a5ba0bd38fdbbc8a
Lizardo <lizardo@utp.edu.co>
2021-10-18 18:27:25 -0500

    Correcciones bucket S3

--- Quetz/settings.py
+++ Quetz/settings.py
@@ -151,9 +151,9 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
 AWS_URL = os.environ.get("AWS_URL")
 AWS_DEFAULT_ACL = None
 AWS_S3_REGION_NAME = "us-east-2"
-AWS_S3_SIGNATURE_VERSION = "s3v4"
+#AWS_S3_SIGNATURE_VERSION = "s3v4"
+AWS_S3_ADDRESSING_STYLE = "virtual"
 
-STATICFILES_STORAGE = "storages.backends.s3boto.S3BotoStorage"
 
 STATIC_URL = AWS_URL + "/static/"
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
